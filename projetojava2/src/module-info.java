718b467f0aa4410e3350374df17f163b4eabe8a8
gilcasacelular <gilcasacelular@outlook.com>
2022-12-20 22:14:34 -0300

    Commit projeto 2

---
+++ projetojava2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Gilberto
+ *
+ */
+module projetojava2 {
+}
---
+++ projetojava2/src/projetojava2/executavel/projetojava2.java
@@ -0,0 +1,9 @@
+package projetojava2.executavel;
+
+public class projetojava2 {
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
